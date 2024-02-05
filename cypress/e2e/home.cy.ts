3f10da9b3054e3e42d846cdad89698ca7cfbc8ca
abraaorochapb <abraaorochapb@gmail.com>
2024-02-05 18:21:44 -0300

    Adicionado particionador context, para separar os testes da homepage

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -6,13 +6,20 @@ describe('Homepage Test', () => {
     cy.visit('http://localhost:3000')
   })
 
-  it('should be able to visit', () => {
-    cy.getByData("hero-heading").should("exist").contains('Testing Next.js Applications with Cypress')
+  context('Hero Section tests', () => {
+
+    it('should be able to visit', () => {
+      cy.getByData("hero-heading").should("exist").contains('Testing Next.js Applications with Cypress')
+    })
+  
+    it('checks if the features on the homepage are correct', () => {
+      cy.get('dt').eq(0).contains("4 Courses")
+      cy.get('dt').eq(1).contains("25+ Lessons")
+      cy.get('dt').eq(2).contains("Free and Open Source")
+    })
+
   })
 
-  it('checks if the features on the homepage are correct', () => {
-    cy.get('dt').eq(0).contains("4 Courses")
-    cy.get('dt').eq(1).contains("25+ Lessons")
-    cy.get('dt').eq(2).contains("Free and Open Source")
-  });
+  
+  
 })
