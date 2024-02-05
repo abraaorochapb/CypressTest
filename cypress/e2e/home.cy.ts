57ca4d1f7e31188efa5d48b60127804a02738964
abraaorochapb <abraaorochapb@gmail.com>
2024-02-05 18:55:09 -0300

    Criados testes de sistema referentes a abertura dos cursos via click em botão referente a cada curso

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -4,7 +4,7 @@ describe('Homepage Test', () => {
 
   beforeEach(() => {
     cy.visit('http://localhost:3000')
-  })
+  });
 
   context('Hero Section tests', () => {
 
@@ -18,8 +18,28 @@ describe('Homepage Test', () => {
       cy.get('dt').eq(2).contains("Free and Open Source")
     })
 
-  })
+  });
 
-  
-  
+  context('Courses Section tests', () => {
+    
+    it('Should be able to open the first course on the page', () => {
+      cy.getByData("course-0").find('a').contains('Get started').click()
+      cy.get('.mt-1 > .block').should('exist')
+      cy.location("pathname").should("equal", "/testing-your-first-application")
+    });
+
+    it('Should be able to open the second course on the page', () => {
+      cy.getByData("course-1").find('a').contains('Get started').click()
+      cy.get('.mt-1 > .block').should('exist')
+      cy.location("pathname").should("equal", "/testing-foundations")
+    });
+
+    it('Should be able to open the third course on the page', () => {
+      cy.getByData("course-2").find('a').contains('Get started').click()
+      cy.get('.mt-1 > .block').should('exist')
+      cy.location("pathname").should("equal", "/cypress-fundamentals")
+    });
+
+  });
+
 })
